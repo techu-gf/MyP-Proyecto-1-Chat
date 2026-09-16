488367fe8e091fa835a34eb1eca8d585afa79700
techu-gf <mayte.garcia.franco@ciencias.unam.mx>
2026-09-16 16:59:41 -0600

    Actualización de métodos/funciones en Mensaje

--- proyecto1/src/main/Modelo/Mensaje/Mensaje.go
+++ proyecto1/src/main/Modelo/Mensaje/Mensaje.go
@@ -14,42 +14,43 @@ type Mensaje struct{
 	Result string `json:"result,omitempty"`
 	Extra string `json:"extra,omitempty"`
 	Status string `json:"status,omitempty"`
-	Users string `json:"users,omitempty"`
+	Users map[string]string `json:"users,omitempty"`
+	Usernames []string `json:"usernames,omitempty"`
 	Text string `json:"text,omitempty"`
 	Roomname string `json:"roomname,omitempty"`
 }
 
-func CrearMensajeIdentify(username string) *Mensaje{
+func CrearMensajeIdentify(username string) *Mensaje {
 	return &Mensaje{
 		Tipo : "IDENTIFY",
 		Username : username,
 	}
 }
 
-func CrearMensajeResponse(operation, result, extra string) *Mensaje{
+func CrearMensajeResponse(operation, result, extra string) *Mensaje {
 	return &Mensaje{
 		Tipo : "RESPONSE",
+		Operation : operation,
 		Result : result,
 		Extra : extra,
-		
 	}
 }
 
-func CrearMensajeNewUser(username string) *Mensaje{
+func CrearMensajeNewUser(username string) *Mensaje {
 	return &Mensaje{
 		Tipo : "NEW_USER",
 		Username : username,
 	}
 }
 
-func CrearMensajeStatus(status string) *Mensaje{
+func CrearMensajeStatus(status string) *Mensaje {
 	return &Mensaje{
 		Tipo : "STATUS",
 		Status : status,
 	}
 }
 
-func CrearMensajeNewStatus(status, username string) *Mensaje{
+func CrearMensajeNewStatus(username, status string) *Mensaje {
 	return &Mensaje{
 		Tipo : "NEW_STATUS",
 		Username : username,
@@ -57,91 +58,180 @@ func CrearMensajeNewStatus(status, username string) *Mensaje{
 	}
 }
 
-func CrearMensajeUsers() *Mensaje{
+func CrearMensajeUsers() *Mensaje {
 	return &Mensaje{
 		Tipo : "USERS",
 	}
 }
 
-func CrearMensajeUSER_LIST(username string) *Mensaje{
+func CrearMensajeUserList(users map[string]string) *Mensaje {
 	return &Mensaje{
-		Tipo : "IDENTIFY",
+		Tipo : "USER_LIST",
+		Users : users,
+	}
+}
+
+func CrearMensajeText(username, text string) *Mensaje {
+	return &Mensaje{
+		Tipo : "TEXT",
+		Username : username,
+		Text : text,
+	}
+}
+
+func CrearMensajeTextFrom(username, text string) *Mensaje {
+	return &Mensaje{
+		Tipo : "TEXT_FROM",
+		Username : username,
+		Text : text,
+	}
+}
+
+func CrearMensajePublicText(text string) *Mensaje {
+	return &Mensaje{
+		Tipo : "PUBLIC_TEXT",
+		Text : text,
+	}
+}
+
+func CrearMensajePublicTextFrom(username, text string) *Mensaje {
+	return &Mensaje{
+		Tipo : "PUBLIC_TEXT_FROM",
+		Username : username,
+		Text : text,
+	}
+}
+
+func CrearMensajeNewRoom(roomname string) *Mensaje {
+	return &Mensaje{
+		Tipo : "NEW_ROOM",
+		Roomname : roomname,
+	}
+}
+
+func CrearMensajeInvite(roomname string, usernames []string) *Mensaje {
+	return &Mensaje{
+		Tipo : "INVITE",
+		Roomname : roomname,
+		Usernames : usernames,
+	}
+}
+
+func CrearMensajeInvitation(username, roomname string) *Mensaje {
+	return &Mensaje{
+		Tipo : "INVITATION",
+		Username : username,
+		Roomname : roomname,
+	}
+}
+
+func CrearMensajeJoinRoom(roomname string) *Mensaje {
+	return &Mensaje{
+		Tipo : "JOIN_ROOM",
+		Roomname : roomname,
+	}
+}
+
+func CrearMensajeJoinedRoom(roomname, username string) *Mensaje {
+	return &Mensaje{
+		Tipo : "JOINED_ROOM",
+		Roomname : roomname,
 		Username : username,
 	}
 }
 
-func (msg *Mensaje) EsValido() bool{
+func CrearMensajeRoomUsers(roomname string) *Mensaje {
+	return &Mensaje{
+		Tipo : "ROOM_USERS",
+		Roomname : roomname,
+	}
+}
+
+func CrearMensajeRoomText(roomname, text string) *Mensaje {
+	return &Mensaje{
+		Tipo : "ROOM_TEXT",
+		Roomname : roomname,
+		Text : text,
+	}
+}
+
+func CrearMensajeRoomTextFrom(roomname, username, text string) *Mensaje {
+	return &Mensaje{
+		Tipo : "ROOM_TEXT_FROM",
+		Roomname : roomname,
+		Username : username,
+		Text : text,
+	}
+}
+
+func CrearMensajeLeaveRoom(roomname string) *Mensaje {
+	return &Mensaje{
+		Tipo : "LEAVE_ROOM",
+		Roomname : roomname,
+	}
+}
+
+func CrearMensajeLeftRoom(roomname, username string) *Mensaje {
+	return &Mensaje{
+		Tipo : "LEFT_ROOM",
+		Roomname : roomname,
+		Username : username,
+	}
+}
+
+func CrearMensajeDisconnect() *Mensaje {
+	return &Mensaje{
+		Tipo : "DISCONNECT",
+	}
+}
+
+func CrearMensajeDisconnected(username string) *Mensaje {
+	return &Mensaje{
+		Tipo : "DISCONNECTED",
+		Username : username,
+	}
+}
+
+func (msg *Mensaje)EsValido() bool {
 	return strings.TrimSpace(msg.Tipo) != ""
 }
 
-func (msg *Mensaje) GetTipo() string {
+func (msg *Mensaje)GetTipo() string {
 	return msg.Tipo
 }
 
-func (msg *Mensaje) GetUsername() string {
+func (msg *Mensaje)GetUsername() string {
 	return msg.Username
 }
 
-func (msg *Mensaje) GetOperation() string {
+func (msg *Mensaje)GetOperation() string {
 	return msg.Operation
 }
 
-func (msg *Mensaje) GetResult() string {
+func (msg *Mensaje)GetResult() string {
 	return msg.Result
 }
 
-func (msg *Mensaje) GetExtra() string {
+func (msg *Mensaje)GetExtra() string {
 	return msg.Extra
 }
 
-func (msg *Mensaje) GetStatus() string {
+func (msg *Mensaje)GetStatus() string {
 	return msg.Status
 }
 
-func (msg *Mensaje) GetUsers() string {
+func (msg *Mensaje)GetUsers() map[string]string {
 	return msg.Users
 }
 
-func (msg *Mensaje) GetText() string {
+func (msg *Mensaje)GetUsernames() []string {
+	return msg.Usernames
+}
+
+func (msg *Mensaje)GetText() string {
 	return msg.Text
 }
 
-func (msg *Mensaje) GetRoomname() string {
+func (msg *Mensaje)GetRoomname() string {
 	return msg.Roomname
-}
-
-func (msg *Mensaje) SetTipo(tipo string) {
-	msg.Tipo = strings.TrimSpace(tipo)
-}
-
-func (msg *Mensaje) SetUsername(username string) {
-	msg.Username = strings.TrimSpace(username)
-}
-
-func (msg *Mensaje) SetOperation(operation string) {
-	msg.Operation = strings.TrimSpace(operation)
-}
-
-func (msg *Mensaje) SetResult(result string) {
-	msg.Result = strings.TrimSpace(result)
-}
-
-func (msg *Mensaje) SetExtra(extra string) {
-	msg.Extra = extra
-}
-
-func (msg *Mensaje) SetStatus(status string) {
-	msg.Status = strings.TrimSpace(status)
-}
-
-func (msg *Mensaje) SetUsers(users string) {
-	msg.Users = users
-}
-
-func (msg *Mensaje) SetText(text string) {
-	msg.Text = text
-}
-
-func (msg *Mensaje) SetRoomname(roomname string) {
-	msg.Roomname = strings.TrimSpace(roomname)
 }
